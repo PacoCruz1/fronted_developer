2998b0f77bc71df20b5ed64974e165c226d202f6
PacoCruz1 <frcruza@hotmail.com>
2022-03-22 23:56:37 +0100

    Prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Equipos de Futbol';
+  title = 'Equipos de liga de Futbol';
 }
